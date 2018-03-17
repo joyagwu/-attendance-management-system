dab6a1707ace7b1211090572a7cfa7a0390b7ba6
joyagwu <37336483+joyagwu@users.noreply.github.com>
2018-03-17 12:10:34 +0100

    more commit

---
+++ eie project/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eie_project.Models;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+
+namespace eie_project.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        string connectionString1 = System.Configuration.ConfigurationManager.ConnectionStrings["projectdb"].ConnectionString;
+        //SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["projectdb"].ConnectionString);
+        // GET: Account
+
+
+        /*public ActionResult Students()
+        {
+            if (ModelState.IsValid)
+            {
+                //String connectionString = "<THE CONNECTION STRING HERE>";
+                String sql = "SELECT * FROM dbo.STUDENTS";
+
+
+                using (SqlConnection conn = new SqlConnection(connectionString1))
+                {
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        conn.Open();
+                        SqlDataReader rdr = cmd.ExecuteReader();
+                        var UserAccount = new List<Student>();
+                        while (rdr.Read())
+                        {
+                            var model = new Student();
+                            model.FirstName = rdr["FirstName"];
+                            model.LastName = rdr["LastName"];
+                            model.Level = rdr["Level"];
+
+
+                            UserAccount.Add(model);
+
+                            ModelState.Clear();
+                         }
+                      }
+                }
+
+            }
+            return View();
+        }
+        */
+        public ActionResult Index()
+        {
+           //using (OurDbContext db = new OurDbContext())
+           //{
+               //return View(db.userAccount.ToList());
+           //}
+            return View();
+        }
+        public ActionResult Enrolment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Enrolment(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                SqlConnection conn = new SqlConnection(connectionString1);
+                conn.Open();
+                SqlCommand command = new SqlCommand("Insert into dbo.STUDENTS values('" +
+                account.LastName + "','" + account.FirstName + "','" + account.MatricNumber + "','" + account.Level + "')", conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                /*using (OurDbContext db = new OurDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+
+                }*/
+                ModelState.Clear();
+                //ViewBag.Message = account.LastName + " " + account.FirstName + " Successfully Enrolled";
+
+            }
+            return View();
+
+        }
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+
+
+        public string connectionString { get; set; }
+
+        public SqlConnection con { get; set; }
+
+        public string ConnectionStrings { get; set; }
+
+        public ActionResult Ta { get; set; }
+    }
+}
---
+++ eie project/Controllers/AdminController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eie_project.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Security;
+
+
+namespace eie_project.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["projectdb"].ConnectionString);
+
+        // GET: Admin 
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult CreateAdmin()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAdmin(AdminAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+
+                conn.Open();
+                SqlCommand command = new SqlCommand("Insert into dbo.ADMIN values('" +
+                account.FirstName + "','" + account.LastName + "','" + account.Email + "','" + account.Username + "','" + account.Password + "','" + account.Status + "')", conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+
+                ModelState.Clear();
+                //ViewBag.Message = account.LastName + " " + account.FirstName + " Successfully Enrolled";
+
+            }
+            return View();
+
+        }
+
+        //Login
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AdminAccount user)
+        {
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select Status, Username, Password FROM dbo.ADMIN WHERE Username = '" + user.Username + "' and Password = '" + user.Password + "';", conn);
+            //int res = 0;
+            SqlDataReader rd = cmd.ExecuteReader();
+            if (rd.Read())
+            {
+
+                Session["Status"] = rd["Status"].ToString();
+                Session["Username"] = rd["Username"].ToString();
+                if (Session["Status"].ToString() == "admin1")
+                {
+                    //Main Administration Section
+                    return RedirectToAction("LoggedIn", "Admin");
+                }
+                else if (Session["Status"].ToString() == "admin2")
+                {
+                    //Sub Administration Section
+                    return RedirectToAction("Index", "Home");
+                }
+
+                conn.Close();
+            }
+            else
+            {
+                ViewData["error"] = "Incorrect Username or Password";
+                return View();
+            }
+            return View();
+        }
+
+
+        public ActionResult LoggedIn()
+        {
+            ViewBag.welcome = Session["FirstName"];
+
+            return View();
+
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Admin");
+        }
+
+        public ActionResult Student()
+        {
+            conn.Open();
+            List<UserAccount> _User = new List<UserAccount>();
+            SqlDataAdapter da = new SqlDataAdapter("select * FROM dbo.STUDENTS", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                _User.Add(new UserAccount()
+                {
+                    UserID = int.Parse(dr[0].ToString()),
+                    LastName = dr["LastName"].ToString(),
+                    FirstName = dr["FirstName"].ToString(),
+                    MatricNumber = dr["MatricNumber"].ToString(),
+                    Level = Int32.Parse(dr["Level"].ToString())
+                });
+            }
+            return View(_User);
+        }
+
+        
+        public ActionResult AdminTable()
+        {
+            conn.Open();
+            List<AdminAccount> _Admin = new List<AdminAccount>();
+            SqlDataAdapter da = new SqlDataAdapter("select * FROM dbo.ADMIN", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                _Admin.Add(new AdminAccount()
+                {
+                    UserID = int.Parse(dr[0].ToString()),
+                    FirstName = dr["FirstName"].ToString(),
+                    LastName = dr["LastName"].ToString(),
+                    
+                    Status = dr["Status"].ToString()
+                });
+            }
+            return View(_Admin);
+        }
+    }
+}
